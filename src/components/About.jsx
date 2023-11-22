2c037a25aec94a6495ac2d79bb64c3dd5db9818d
supriyomaji <akashmaji200680@gmail.com>
2023-11-22 22:36:53 +0530

    Add existing project files to Git

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,20 @@ const About = () => {
 
       <div className="w-full md:w-2/4 text-center space-y-2">
         <Heading title1="About" title2="Us?" />
+        <p className=" text-lightText"><img src="https://i.ibb.co/6HZtWYg/tinywow-profile-photo-40994231-1.png" className="png1" />
+        
+        <h1 className= "animate-pulse">PRITAM MAZUMDAR<h1 className= "animate-bounce" color="red">FOUNDER & MENTOR</h1> </h1>
+        </p>
+        <p className=" text-lightText"><img src="https://i.ibb.co/j5QFDs3/tinywow-profile-photo-40989981-2.png" className="png1" />
+          <h1 className= "animate-pulse">SHREYA MUKHERJEE <h1 className= "animate-bounce" color="red">APP ARCHITECT</h1> </h1>
+        </p>
+
         <p className=" text-lightText"><img src="https://avatars.githubusercontent.com/u/122034818?s=400&u=b523d382d0aab4c8cb177f1f31debf7e7d99a5e3&v=4"className="png1" />
         <h1 className= "animate-pulse">SUPRIYO MAJI <h1 className= "animate-bounce" color="red">WEB WIZARD</h1> </h1>
         </p>
         
-          <p className=" text-lightText"><img src="https://i.ibb.co/sHCTzty/tinywow-profile-photo-40989981-2.png" className="png1" />
-          <h1 className= "animate-pulse">SHREYA MUKHERJEE <h1 className= "animate-bounce" color="red">APP ARCHITECT</h1> </h1>
-        </p>
-        <p className=" text-lightText"><img src="https://i.ibb.co/XjQdXys/tinywow-profile-photo-40988806-1.png" className="png1" />
+         
+        <p className=" text-lightText"><img src="https://i.ibb.co/tZRNmQm/tinywow-profile-photo-40988806-1.png" className="png1" />
         <h1 className= "animate-pulse">SUPRIYA GARH <h1 className= "animate-bounce" color="red">INSIGHT ILLUMINATOR </h1> </h1>
 
           
@@ -32,11 +38,7 @@ const About = () => {
 
           
         </p>
-        <p className=" text-lightText"><img src="https://i.ibb.co/6HZtWYg/tinywow-profile-photo-40994231-1.png" className="png1" />
-        
-        <h1 className= "animate-pulse">PRITAM MAZUMDAR<h1 className= "animate-bounce" color="red">FOUNDER & MENTOR</h1> </h1>
-        </p>
-
+       
         <Link to="contact" spy={true} smooth={true} duration={500}>
           <Button title="Contact Us" />
         </Link>
